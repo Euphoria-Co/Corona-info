300ba66f01522ac22fab22d0f7a72144461440e3
ViciousCircle-Github <Stinnnkey0@gmail.com>
2020-03-20 16:44:53 +0100

    async all images added

--- App.js
+++ App.js
@@ -32,7 +32,17 @@ const images = [
   require("./assets/images/illustration_2.png"),
   require("./assets/images/illustration_3.png"),
   require("./assets/images/avatar.png"),
-  require("./assets/images/objawy.png")
+  require("./assets/images/1.png"),
+  require("./assets/images/2.png"),
+  require("./assets/images/3.png"),
+  require("./assets/images/4.png"),
+  require("./assets/images/5.png"),
+  require("./assets/images/6.png"),
+  require("./assets/images/7.png"),
+  require("./assets/images/8.png"),
+  require("./assets/images/9.png"),
+  require("./assets/images/10.png"),
+  require("./assets/images/11.png"),
 
 ];
 
